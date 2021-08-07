880b9011a7ba4e1042d3ff7d865b04e7178379e0
Anatolii Bobrovskiy <tolekbob2017@gmail.com>
2021-08-08 01:31:43 +0300

    HW 10

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import cards from './menu.json';
+import menuCardsTpl from './templates/menuCardsTpl.hbs';
+import {onChangeCheckbox} from './checkbox';
+
+const menuCards = menuCardsTpl(cards);
+const menuRender = document.querySelector('.js-menu');
+const checkboxRef = document.querySelector('#theme-switch-toggle');
+
+savedThemeLoad ();
+
+menuRender.insertAdjacentHTML('afterbegin', menuCards);
+checkboxRef.addEventListener('change', onChangeCheckbox);
+
+function savedThemeLoad () {
+    const savedTheme = localStorage.getItem('Theme');
+    const bodyRef = document.querySelector('body');
+    if (savedTheme) {
+        bodyRef.classList.add(savedTheme);
+        if (savedTheme === 'dark-theme') {
+            checkboxRef.checked = true;
+        };
+    };
+}
